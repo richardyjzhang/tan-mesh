0081d0df36bdd101a5f79304f127626f81c19dee
zhangyj <richard.yujia.zhang@gmail.com>
2020-02-21 12:26:10 +0800

    getter应为const

---
+++ tanvector.cpp
@@ -0,0 +1,76 @@
+#include "tanvector.h"
+#include <cmath>
+
+double TanVector::_TOLERANCE_ = 0.001;
+
+TanVector::TanVector()
+    : x(0), y(0), z(0) { }
+
+TanVector::TanVector(double x, double y, double z)
+    : x(x), y(y), z(z) { }
+
+void TanVector::setTolerance(double tolerance) { _TOLERANCE_ = tolerance; }
+
+double TanVector::getX() const { return x; }
+
+double TanVector::getY() const { return y; }
+
+double TanVector::getZ() const { return z; }
+
+void TanVector::setX(double x) { this->x = x; }
+
+void TanVector::setY(double y) { this->y = y; }
+
+void TanVector::setZ(double z) { this->z = z; }
+
+double TanVector::length() const
+{
+    return std::sqrt(x * x + y * y + z * z);
+}
+
+double TanVector::distanceTo(const TanVector & vec) const
+{
+    return std::sqrt((x - vec.x) * (x - vec.x) +
+                     (y - vec.y) * (y - vec.y) +
+                     (z - vec.z) * (z - vec.z));
+}
+
+TanVector TanVector::normalize() const
+{
+    TanVector vec;
+    double len = length();
+    if (len > _TOLERANCE_)
+    {
+        vec.x = x / len;
+        vec.y = y / len;
+        vec.z = z / len;
+    }
+
+    return vec;
+}
+
+bool operator < (const TanVector & vec1, const TanVector & vec2)
+{
+    if (vec1.x < vec2.x) return true;
+    if ((std::abs(vec1.x - vec2.x) < TanVector::_TOLERANCE_) && (vec1.y < vec2.y)) return true;
+    if ((std::abs(vec1.y - vec2.y) < TanVector::_TOLERANCE_) && (vec1.z < vec2.z)) return true;
+    return false;
+}
+
+TanVector operator + (const TanVector & vec1, const TanVector & vec2)
+{
+    TanVector vec(vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z);
+    return vec;
+}
+
+TanVector operator - (const TanVector & vec1, const TanVector & vec2)
+{
+    TanVector vec(vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z);
+    return vec;
+}
+
+TanVector operator * (double scale, const TanVector & vec)
+{
+    TanVector v(scale * vec.x, scale * vec.y, scale * vec.z);
+    return v;
+}
--- tanvector.h
+++ tanvector.h
@@ -1,4 +1,40 @@
 #ifndef TANVECTOR_H
 #define TANVECTOR_H
 
+class TanVector
+{
+public:
+    TanVector();
+    TanVector(double x, double y, double z);
+
+private:
+    static double _TOLERANCE_;
+    double x;
+    double y;
+    double z;
+
+public:
+    static void setTolerance(double tolerance);
+    double getX() const;
+    double getY() const;
+    double getZ() const;
+    void setX(double x);
+    void setY(double y);
+    void setZ(double z);
+
+public:
+    double length() const;
+    TanVector normalize() const;
+    double distanceTo(const TanVector & vec) const;
+    friend bool operator < (const TanVector & vec1, const TanVector & vec2);
+    friend TanVector operator + (const TanVector & vec1, const TanVector & vec2);
+    friend TanVector operator - (const TanVector & vec1, const TanVector & vec2);
+    friend TanVector operator * (double scale, const TanVector & vec);
+};
+
+bool operator < (const TanVector & vec1, const TanVector & vec2);
+TanVector operator + (const TanVector & vec1, const TanVector & vec2);
+TanVector operator - (const TanVector & vec1, const TanVector & vec2);
+TanVector operator * (double scale, const TanVector & vec);
+
 #endif // TANVECTOR_H
